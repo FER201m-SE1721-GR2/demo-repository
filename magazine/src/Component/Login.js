23d2f999bb832dc03497f5db5a918a1ed6108fff
Long Hai <127493031+long110902kk@users.noreply.github.com>
2023-07-19 00:09:31 +0700

    Update Login.js

--- magazine/src/Component/Login.js
+++ magazine/src/Component/Login.js
@@ -1,81 +1,121 @@
-import { useEffect, useState } from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
-import { Link, json, useNavigate, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import { useNavigate } from 'react-router-dom'
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
 
-    const [id, setid] = useState("");
-    const [user, setuser] = useState('');
-    const [password, setpassword] = useState('');
-    const [validation, valichange] = useState(false);
-    const { userid } = useParams();
-    const nagivate = useNavigate();
+    const [username, setUsername] = useState('')
+    const [password, setPassWord] = useState('');
+    const [users, setUsers] = useState([]);
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        sessionStorage.clear();
+    }, []);
 
 
-    const handleSubmit = (e) => {
+    useEffect(() => {
+
+        fetch(" http://localhost:9999/users").then((res) => res.json())
+            .then((data) => {
+                setUsers(data)
+            }).catch(err => {
+                console.log(err.message)
+            })
+    }, [])
+
+    const ProceedLogin = (e) => {
         e.preventDefault();
-        const studata = ({ id, user, password });
-        fetch("http://localhost:9999/new_users", {
-            method: "Post",
-            headers: { "content-type": "application/json" },
-            body: JSON.stringify(studata)
-        }).then((res) => {
-            alert("Login successfully!")
-            nagivate('/')
-        }).catch((err) => {
-            console.log(err.message);
-        })
-    };
+        if (validate()) {
+
+            let temp = [...users];
+            let currentUser = temp.filter(user => user.username === username)
+
+            if (currentUser.length == 0 || currentUser === undefined) {
+                toast.error('Username does not exist! ')
+                setPassWord('')
+            } else {
+                fetch("http://localhost:9999/users/" + currentUser[0].id).then((res) => {
+
+                    return res.json();
+                }).then((resp) => {
+
+                    if (Object.keys(resp).length === 0) {
+                        toast.error('Please Enter username');
+                    } else {
+                        if (resp.password === password) {
+                            toast.success('Success');
+                            sessionStorage.setItem('uid', resp.id);
+                            sessionStorage.setItem('userrole', resp.rId);
+                            sessionStorage.setItem('uName', resp.username)
+                            navigate('/')
+                            window.location.reload();
+                        } else {
+                            setPassWord('')
+                            toast.error('Please Enter valid credentials');
+                        }
+                    }
+                }).catch((err) => {
+                    toast.error('Login Failed due to :' + err.message);
+                });
+            }
+        }
+
+    }
+
+
+    const validate = () => {
+        let result = true;
+        if (username === '' || username === null) {
+            result = false;
+            toast.warning('Please Enter username');
+        }
+        if (password === '' || password === null) {
+            result = false;
+            toast.warning('Please Enter Password');
+        }
+        return result;
+    }
+
+
+
 
     return (
-        <Row>
-            <Container class="vh-100">
-                <Row>
-                    <Col xs={12}>
-                        <section >
-                            <div class="container py-5 h-100" >
-                                <div class="row d-flex justify-content-center align-items-center h-100">
-                                    <div class="col-12 col-md-8 col-lg-6 col-xl-5" style={{ marginTop: "80px" }} >
-                                        <div class="card shadow-2-strong" style={{ borderRadius: "1rem" }}>
-                                            <div class="card-body p-5 text-center">
-                                                <h3 class="mb-5">Sign in</h3>
-                                                <div className="card-body">
-                                                    <div className="row">
-                                                        <div className="col-lg-12">
-                                                            <div className="form-group">
-                                                                <label>Username</label>
-                                                                <input required value={user} onMouseDown={e => valichange(true)} onChange={e => setuser(e.target.value)} className="form-control"></input>
-                                                                {user.length == 0 && validation && <span className="text-danger">Please enter the username</span>}
-                                                            </div>
-                                                        </div>
+        <div className="background_login d-flex justify-content-center align-items-center " >
+            <ToastContainer />
 
-                                                        <div className="col-lg-12">
-                                                            <div className="form-group">
-                                                                <label>Password</label>
-                                                                <input required value={password} onMouseDown={e => valichange(true)} onChange={e => setpassword(e.target.value)} className="form-control"></input>
-                                                                {user.length == 0 && validation && <span className="text-danger">Please enter the password</span>}
-                                                            </div>
-                                                        </div>
 
-                                                        <div className="col-lg-12">
-                                                            <div className="form-group">
-                                                                <button type="submit" className="btn btn-success">Login</button>
-                                                                <span> | </span>
-                                                                <Link to="/register" style={{ "text-decoration": "underline" }}>Register</Link>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                    </Col>
-                </Row>
-            </Container>
-        </Row>
+            <form style={{ backgroundColor: 'white', marginTop: '100px', padding: '80px', borderRadius: '20px' }} onSubmit={ProceedLogin}>
+                <h1 style={{ textAlign: 'center', paddingBottom: '30px', textTransform: 'uppercase' }}>
+                    Login
+                </h1>
+                <div className="form-outline mb-4">
+                    <label className="form-label" for="form2Example1">Username</label>
+                    <input type="text" id="form2Example1" class="form-control"
+                        value={username} onChange={e => setUsername(e.target.value)} />
+
+                </div>
+
+
+                <div className="form-outline mb-4">
+                    <label class="form-label" for="form2Example2" >Password</label>
+                    <input type="password" id="form2Example2" class="form-control"
+                        value={password} onChange={e => setPassWord(e.target.value)} />
+
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block mb-4" >Sign in</button>
+
+
+                <div className="text-center">
+                    <p>Not a member? <a href="/register">Register</a></p>
+
+                </div>
+            </form>
+
+        </div>
     );
 }
 
